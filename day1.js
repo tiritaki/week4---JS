fa7bda7e1538ccaacb77618bc1f35c449bd8303c
Oleh Pysmenko <olehpysmenko@blessing.local>
2022-08-09 16:17:19 +0100

    day2

---
+++ day1.js
@@ -0,0 +1,40 @@
+var number = 5;
+console.log(number);
+
+let a = 5;
+let b = 6;
+let c = 6;
+
+console.log(a);
+
+
+
+let day = 9
+
+switch (day) {
+  case 7:
+    console.log("It's Sunday!");
+    break;
+  case 1:
+    console.log("It's Monday");
+    break;
+  case 2:
+    console.log("It's Tuesday");
+    break;
+  case 3:
+    console.log("It's Wednesday");
+    break;
+  case 4:
+    console.log("It's Thursday");
+    break;
+  case 5:
+    console.log("It's Friday");
+    break;
+  case 6:
+    console.log("It's Saturday");
+    break;
+  default:
+    console.log("pls select another number");
+    break;
+}
+
